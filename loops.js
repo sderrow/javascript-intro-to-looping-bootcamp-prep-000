b35dd6d373fadc47a4322b3c309f3c65cbcb1b92
Sean Derrow <sderrow@mit.edu>
2017-11-09 19:43:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,3 +4,13 @@ function whileLoop(n) {
   }
   console.log('done')
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
+function doWhileLoop(myArray) {
+  do {
+    myArray.pop()
+  } while ( myArray.length > 0 && maybeTrue() )
+}
